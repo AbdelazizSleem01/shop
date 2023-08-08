405542877a2a7f9f238c44b2f808e3a53e9e4a90
AbdelazizSleem01 <134790992+AbdelazizSleem01@users.noreply.github.com>
2023-08-08 21:11:49 +0300

    EDIT IN JS HTML  CSS

--- scripts.js
+++ scripts.js
@@ -53,23 +53,62 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
-      .forEach(function (form) {
+     .forEach(function (form) {
         form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
+            if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
+        }
+        form.classList.add('was-validated')
         }, false)
-      })
-  })()
+    })
+})();
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxtq1jCv2H0-OfQ6Gs4xwls3bsGP974-DsefW9ofpCZZhtjpMc3coo1tqR9UBo_FY9i/exec'
+const form = document.querySelector('#form')
+const btn = document.querySelector('#submit')
+
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+btn.disabled = true
+btn.innerHTML = "Loading..."
+
+console.log(form)
+fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => { 
+        btn.disabled = false
+btn.innerHTML = "Submit"
+        alert('Success!', response) })
+    .catch(error => {
+        btn.disabled = false
+btn.innerHTML = "Submit"
+        alert('Error!', error.message)})
+})
+
+
+const conURL = 'https://script.google.com/macros/s/AKfycbztTnN8p0qIrHoPhP3Bs9A202DG0ZdEEY1ODFqTCJgNtURGjaSIFyX9f7Z52VNKeshW/exec'
+const formcont = document.querySelector('#conForm')
+const conbtn = document.querySelector('#consubmit')
+
+
+formcont.addEventListener('submit', e => {
+e.preventDefault()
+conbtn.disabled = true
+conbtn.innerHTML = "Loading..."
+
+console.log(formcont)
+fetch(conURL, { method: 'POST', body: new FormData(formcont) })
+    .then(response => { 
+        conbtn.disabled = false
+        conbtn.innerHTML = "Submit"
+        alert('A successful message will be reviewed !', response) })
+    .catch(error => {
+        conbtn.disabled = false
+        conbtn.innerHTML = "Submit"
+        alert('Error!', error.message)})
+})
